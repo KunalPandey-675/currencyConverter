652e94fded19b7f36e5eeb846de7ed9a27b1c9ba
KunalPandey-675 <pandeykunal2084@gmail.com>
2025-01-09 00:18:51 +0530

    images

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }
 
   return (
-    <div className="bg-[url('./public/bg.jpg')] h-screen flex flex-col items-center justify-center gap-8">
+    <div className="bg-[url('/bg.jpg')] h-screen flex flex-col items-center justify-center gap-8">
       <div className="absolute inset-0 bg-black/30"></div>
       <h1 className='text-4xl font-extrabold text-white z-10 '>Currency Convertor</h1>
       <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
@@ -48,7 +48,7 @@ function App() {
               className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white bg-blue-600 text-white p-2 rounded-full"
               onClick={swap}
             >
-              <img className='h-7' src="./public/swap.svg" alt="swap" />
+              <img className='h-7' src="/swap.svg" alt="swap" />
             </button>
           </div>
           <div className="w-full mt-1 mb-4">
